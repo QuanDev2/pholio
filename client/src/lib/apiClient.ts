ab8212f4cae59b1bcbdd7e096754e1ca8ae15062
Quan <quancfc17@gmail.com>
2026-07-01 19:39:28 -0400

    axios interceptors

--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,6 +1,37 @@
 import axios from "axios";
+import { getAccessToken, setAccessToken } from "./authToken";
 
 export const api = axios.create({
   baseURL: "http://localhost:4000/api/v1",
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response, // success path, pass thru untouched
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const res = await axios.post(
+          "http://localhost:4000/api/v1/auth/refresh",
+          {},
+          { withCredentials: true },
+        );
+        setAccessToken(res.data.token);
+        return api(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
